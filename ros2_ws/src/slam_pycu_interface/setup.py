523cdf0bf53846ad3ba634dd27f9fc538bfac746
MichaelXzj <45219543+MichaelXiongZJ@users.noreply.github.com>
2025-11-15 19:32:48 -0800

    fix

--- ros2_ws/src/slam_pycu_interface/setup.py
+++ ros2_ws/src/slam_pycu_interface/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'slam_pycu_interface'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
